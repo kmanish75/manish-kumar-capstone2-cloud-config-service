6715eca2577e6ba65ce623dd6352c64595c75aa5
Manish <kmanish75@gmail.com>
2019-09-11 10:53:23 -0500

    capstone2 cloud config service first commit

---
+++ src/main/java/com/trilogyed/capstone2cloudconfigservice/Capstone2CloudConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.capstone2cloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone2CloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capstone2CloudConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilogyed/capstone2cloudconfigservice/Capstone2CloudConfigServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trilogyed.capstone2cloudconfigservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Capstone2CloudConfigServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
