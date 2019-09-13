7427a09712a721f1e4f436da3094b72067d3e0d4
Manish <kmanish75@gmail.com>
2019-09-13 17:33:16 -0500

    second commit

--- src/main/java/com/trilogyed/capstone2cloudconfigservice/Capstone2CloudConfigServiceApplication.java
+++ src/main/java/com/trilogyed/capstone2cloudconfigservice/Capstone2CloudConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.trilogyed.capstone2cloudconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Capstone2CloudConfigServiceApplication {
 
 	public static void main(String[] args) {
